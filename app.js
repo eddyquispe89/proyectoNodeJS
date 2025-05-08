2963962bc1bd605e924c88b209a695ed7eac7f97
eddyquispeh <eddy.silvestre.quispe.huaman1@kyndryl.com>
2025-05-08 11:38:01 -0500

    actualizando codigo

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const path = require('path'); // Para manejar rutas de archivos
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+// Establecer EJS como motor de plantillas
+app.set('view engine', 'ejs');
+
+// Servir archivos estáticos (como imágenes) desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta principal
 app.get('/', (req, res) => {
-  res.send('¡Hola, Azure! Mi aplicación Node.js está corriendo!');
+  res.render('index', { 
+    title: 'Clase de APP Service - WME', 
+    description: 'Capacitación sobre APP Service para el equipo Kyndryl', 
+    imageUrl: '/images/kyndryl-logo.png' // Asegúrate de tener la imagen en la carpeta 'public/images'
+  });
 });
 
 // Inicia el servidor
