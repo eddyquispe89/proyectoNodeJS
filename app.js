a56076a7f2f12b9845e369240d95e177a127b85c
eddyquispeh <eddy.silvestre.quispe.huaman1@kyndryl.com>
2025-05-10 17:40:54 -0500

    añadir error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // Establecer EJS como motor de plantillas
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // Servir archivos estáticos (como imágenes) desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,6 +16,12 @@ app.get('/', (req, res) => {
     description: 'Capacitación sobre APP Service para el equipo Kyndryl', 
     imageUrl: '/images/kyndryl-logo.png' // Asegúrate de tener la imagen en la carpeta 'public/images'
   });
+});
+
+// Middleware de manejo de errores (si ocurre un error)
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Esto debería registrar el error en la consola
+  res.status(500).send('Something broke!'); // Esto envía una respuesta de error 500 al cliente
 });
 
 // Inicia el servidor
